48e7101dcb6948bbcfae5593daaef7f7c122f5dc
jipmoon <hyungjip@Hyungs-MacBook-Pro-2.local>
2016-10-23 23:03:39 -0700

    Swift playground

---
+++ Exercise1.playground/Contents.swift
@@ -0,0 +1,36 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var num1 = 15
+
+var num2 = 5
+
+var num3 = num1 + num2
+
+var num4 = num1 * num2
+
+var num5 = num1 - num2
+
+var num6 = num1 / num2
+
+var num7 = 1, num8 = 2, num9 = 3
+
+let num10 = 4, num11 = 5, num12 = 6
+
+var num13: Double
+
+var num14: Double = 423
+
+var num15: String = "Hello"
+
+var num16: String = "World"
+
+var num17 = num15 + num16
+
+var num18 = "\(num15) \(num16)"
+
+
+
+
+
---
+++ Numbers.playground/Contents.swift
@@ -0,0 +1,11 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//Numbers
+//Type inference
+var age = 30
+
+//Explicitly declared type
+var weight: Int = 200 //Int
+
